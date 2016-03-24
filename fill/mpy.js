783e7a06df6390720fc668ef4236a1b47664cc3c
Johan Gyllenspetz <johan.gyllenspetz@gmail.com>
2016-03-24 16:52:21 -0700

    Add fill example

---
+++ fill/mpy.js
@@ -0,0 +1,11 @@
+var mly = new Mapillary.Viewer(
+    'mly',
+    'cjJ1SUtVOEMtdy11b21JM0tyYTZIQTo2ZmVjNTQ3YWQ0OWI2Yjgx',
+    'tJZWhjD_55y4bH9r8q2XiQ',
+    {
+        "cover": false,
+        "baseImageSize": Mapillary.ImageSize.Size2048,
+        "maxImageSize": Mapillary.ImageSize.Size2048,
+    });
+
+mly.setRenderMode(Mapillary.RenderMode.Fill);
